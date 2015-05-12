6d0bb830342f58c3659ab9e5955a9bb0d231058c
Mike <mike@prime31.com>
2015-05-11 23:12:53 -0700

    adding all the pods to make this easier to build

---
+++ quark-shell-mac-master/Pods/Headers/Private/LaunchAtLoginController/LaunchAtLoginController.h
@@ -0,0 +1,1 @@
+../../../LaunchAtLoginController/LaunchAtLoginController.h
---
+++ quark-shell-mac-master/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,38 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ISO8601DateFormatter
+#define COCOAPODS_POD_AVAILABLE_ISO8601DateFormatter
+#define COCOAPODS_VERSION_MAJOR_ISO8601DateFormatter 0
+#define COCOAPODS_VERSION_MINOR_ISO8601DateFormatter 7
+#define COCOAPODS_VERSION_PATCH_ISO8601DateFormatter 0
+
+// LaunchAtLoginController
+#define COCOAPODS_POD_AVAILABLE_LaunchAtLoginController
+#define COCOAPODS_VERSION_MAJOR_LaunchAtLoginController 1
+#define COCOAPODS_VERSION_MINOR_LaunchAtLoginController 0
+#define COCOAPODS_VERSION_PATCH_LaunchAtLoginController 0
+
+// MASShortcut
+#define COCOAPODS_POD_AVAILABLE_MASShortcut
+#define COCOAPODS_VERSION_MAJOR_MASShortcut 1
+#define COCOAPODS_VERSION_MINOR_MASShortcut 2
+#define COCOAPODS_VERSION_PATCH_MASShortcut 3
+
+// RHPreferences
+#define COCOAPODS_POD_AVAILABLE_RHPreferences
+#define COCOAPODS_VERSION_MAJOR_RHPreferences 1
+#define COCOAPODS_VERSION_MINOR_RHPreferences 0
+#define COCOAPODS_VERSION_PATCH_RHPreferences 0
+
+// Sparkle
+#define COCOAPODS_POD_AVAILABLE_Sparkle
+#define COCOAPODS_VERSION_MAJOR_Sparkle 1
+#define COCOAPODS_VERSION_MINOR_Sparkle 8
+#define COCOAPODS_VERSION_PATCH_Sparkle 0
+
